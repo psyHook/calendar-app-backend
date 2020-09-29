1ed8d8e4786185df0e900ddf0493779dcde0b8fe
juan galarce <j6alarce@gmail.com>
2020-09-29 18:09:49 -0300

    first commit y configuracion de express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+  res.json({
+    ok: true
+  });
+});
+
+// Escuchar peticiones
+app.listen( 4000, () => {
+  console.log('Servidor corriendo en http://localhost:4000/');
+});
