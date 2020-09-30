26549c043606580ef8f53cc9a39d99c544210858
juan galarce <j6alarce@gmail.com>
2020-09-30 10:56:37 -0300

    Recuperando informacion del body

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,37 @@
 const { response } = require('express');
 
 const createUser = (req, res = response) => {
+  // req: lo que el usuario solicita
+  // res: lo que nosotros le enviamos
+
+  // console.log(req)
+
+  const { name, email, password } = req.body;
+
+  if (name.length < 3) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe de ser de 3 letras',
+    });
+  }
+
   res.json({
     ok: true,
     msg: 'registro',
+    name,
+    email,
+    password,
   });
 };
 
 const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
     msg: 'login',
+    email,
+    password,
   });
 };
 
--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const app = express();
 // Directorio Publico
 app.use( express.static('public') );
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Rutas
 // TODO: auth // crear, login, renew
 app.use('/api/auth', require('./routes/auth') );
+
 // TODO: CRUD: Eventos
 
 // Escuchar peticiones
